772323f1e6be79a0b4e853718b5c2d9a178e09f3
Christopher Brownlee <premier@Tsars-Hall.local>
2020-04-08 18:04:34 -0400

    Pybank updated

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,13 +5,41 @@ budgetCSV = os.path.join("Resources", "budget_data.csv")
 
 with open(budgetCSV, newline="") as budgetFile:
     csvreader = csv.reader(budgetFile, delimiter=",")
-
-    # Getting the number of dates in the CSV
+    #Setting variables that will be needed
     dateNum = 0
+    profitloss = 0
+    profits = []
+    dates = []
+    
+    next(csvreader)
+    # Getting all values needed from the row
+    print(csvreader)
+
     for row in csvreader:
         dateNum = dateNum + 1
-    #Accounts for the header when counting
-    dateNum = dateNum -1
+        profitloss = int(row[1]) + profitloss
+        profits.append(row[1])
+        dates.append(row[0])
 
-    
-    print (dateNum)
+    maxprofits = max(profits)
+    maxloss = min(profits)
+    maxdate = dates[profits.index(maxprofits)]
+    mindate = dates[profits.index(maxloss)]
+
+    # print(maxprofits)
+    profitlossAverage = "${:,.2f}".format(profitloss / dateNum)
+    losses = "${:,.2f}".format(profitloss)
+    #Creating the text output file
+    file = open("output.txt", "w")
+    file.write("Financial Review from 2010 to 2017 \n")
+    file.write("------------------------------------ \n")
+    file.write(f"Total Months Covered {dateNum} \n")
+    file.write(f"Total Amount Made: {losses} \n")
+    file.write(f"Monthly Average: {profitlossAverage} \n")
+    file.write(f"The largest increase of profits came in {maxdate} bringing in {maxprofits} \n")
+    file.write(f"The largest decrease was in {mindate} resulting in losses of {maxloss} \n")
+    file.close()
+    #Final Message delivered    
+    file = open("output.txt", "r")
+
+    print (file.read())
